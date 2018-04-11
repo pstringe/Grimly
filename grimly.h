afc0ea89ab70059cb662af5b28bbe8144c3fab62
Poitier Stringer <pstringe@e1z4r11p7.42.us.org>
2018-04-10 18:49:10 -0700

    lost files

--- grimly.h
+++ grimly.h
@@ -6,13 +6,35 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/09 19:31:04 by pstringe          #+#    #+#             */
-/*   Updated: 2018/04/10 09:46:17 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/04/10 14:53:24 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GRIMLY_H
 # define GRIMLY_H
 # include "libft.h"
+
+/*
+**	used to quickly locate neiboring positions on the map;
+*/
+
+g_r = {-1, 0, 0, 1};
+g_c = {1, 0, -1, 0};
+
+/*
+**	struct to store distances of ponits from entrance
+*/
+
+typedef	struct	s_p
+{
+	int x;
+	int	y;
+	int	d;
+}				t_p;
+
+/*
+**	struct to store relvant map data
+*/
 
 typedef struct	s_m
 {
@@ -24,5 +46,7 @@ typedef struct	s_m
 	char	enter;
 	char	exit;
 	char	**map;
+	int		**v;
 }				t_m;
+
 #endif
