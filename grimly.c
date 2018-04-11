6dac341f6f3ebe1a8a255f034f147fce1595e85e
Poitier Stringer <pstringe@e1z1r5p1.42.us.org>
2018-04-11 15:28:01 -0700

    I have written out my full concept, now I  need only test and debug until it works

--- grimly.c
+++ grimly.c
@@ -6,11 +6,15 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/09 19:42:59 by pstringe          #+#    #+#             */
-/*   Updated: 2018/04/10 19:40:50 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/04/11 15:27:17 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "grimly.h"
+
+/*
+**	inititalizes a 2D int array to track visted cells
+*/
 
 int		**init_v(t_m *m)
 {
@@ -31,41 +35,136 @@ int		**init_v(t_m *m)
 	return(v);
 }
 
-int		solve(t_m *m, t_node *src)
+/*
+**	creates a new point struct and attatches it to its parent
+*/
+
+t_pnt	*pnt_new(t_p *p, int y, int x)
 {
-	t_q		*q;
+	t_p		*pnt;
+
+	pnt = ft_memalloc(sizeof(t_p));
+	pnt->d = p->d + 1;
+	pnt->pa = p;
+	pnt->x = x;
+	pnt->y = y;
+	return (pnt);
+}
+
+/*
+**	queues the neigboring cells that have not been visited.
+*/
+
+t_pnt	*queue_neighbors(t_m *m, t_list *cur)
+{
+	int		i;
+	int		x;
+	int		y;
 	t_pnt	*c;
+	t_list	*n;
+
+	q = m->q;
+	c = (t_p*)(cur->content);
+	i = -1;
+	while (++i < 4)
+	{
+		y = c->y + r[i];
+		x = c->x + c[i];
+		if (y < m->r_n && x < m->c_n)
+		{
+			n = ft_lstnew(&q, pnt_new(c, y, x), sizeof(t_p));
+			ft_lstadd_tail(&(m->q), n);
+			m->v[y][x] = 1;
+		}
+	}
+}
+
+/*
+**	creates a tree of paths and returns the end node of the destination,
+**	or NULL if there is no destination.
+*/
+
+t_pnt	*solve(t_m *m, t_node *src)
+{
+	t_list	*q;
+	t_list	*cur;
+	t_p		*tmp;
 
 	m->v = init_v(m);
 	q = ft_lstnew(src, sizeof(src));
-	c = src;
-	while (ft_lstlen(q))
+	c = q;
+	while (cur)
 	{
-		c = (t_p*)ft_queue_pop(q);
 		if(m->map[c->x][c->y] == m->exit)
-			return (c->d);
-		while (ft_lstlen(q->lst))
-		{
-			
-		}
+			return ((t_p*)(c->content));
+		queue_neighbors(&m, &c);
+		c = c->next;
 	}
-
+	return (NULL);
 }
+
+/*
+**	identifies the position of the entrance and returns it as a t_p
+** 	or NULL for invalid/ non-existant dst.
+*/
+
+t_p		*ident_src(t_m *m)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	j = 0;
+	while (++i < m->r_n)
+		if (m->map[i][j] == m->enter)
+			return (pnt_new(NULL, i, j, 0));
+	while (j++ < m->c_n)
+		if (m->map[i][j] == m->enter)
+			return (pnt_new(NULL, i, j, 0));
+	while (--i >= 0)
+		if (m->map[i][j] == m->enter)
+			return (pnt_new(NULL, i, j, 0));
+	while (++i >= 0)
+		if (m->map[i][j] == m->enter)
+			return (pnt_new(NULL, i, j, 0));
+	return (NULL);
+}
+
+/*
+**	draws the path on the map from dst to src
+*/
+
+void	put_path(t_m *m, t_p *dst)
+{
+	t_p		*tmp;
+
+	tmp = dst->pa;
+	while (tmp)
+	{
+		m->map[tmp->y][tmp->x] = m->path;
+		tmp = tmp->pa;
+	}
+}
+
+/*
+**	parses, validates, solves, and prints a map from valid file descriptors given 
+**	input, will display error messages appropriatly.
+*/
 
 void	grimly(int fd)
 {
 	t_m		maze;
+	t_p		dst;
 
 	if (get_maze(fd, &maze) < 0)
 		ft_putendl("MAP ERROR");
-	else
-		put_maze(maze, 0);
-	/*
-	if (solve(&maze))
-		put_map(maze.map);
-	else
+	else if (!(d = solve(&maze, ident_src(maze))));
 		ft_putendl("MAP ERROR");
-	*/
+	else
+	{
+		put_path(&maze, d);
+		put_maze(maze, 0);
+	}
 }
 
 int		main(int argc, char **argv)
